971b1f638556daedd8cc8239e101e5d98e6e5bd5
Rsurendar2712 <rsurendar116@gmail.com>
2026-09-23 17:02:29 +0530

    Updated files and fixed code bugs

--- src/Compounds/Display.js
+++ src/Compounds/Display.js
@@ -1,67 +1,66 @@
 import React, { useRef } from "react";
 import "./Display.css";
-import video from "../asset/Fvid.mp4";
 
-<<<<<<< HEAD
+import video from "../asset/Fvid.mp4";
 import logo1 from "../asset/im1.jpg";
 import logo2 from "../asset/im2.jpg";
 import logo3 from "../asset/im5.jpg";
 import logo4 from "../asset/im3.jpg";
 import logo5 from "../asset/im4.jpg";
 import logo6 from "../asset/im6.jpg";
-=======
-import logo from "../asset/logo.jpg";
->>>>>>> 5d060767d82e39a89ca2bb7ef288f5a8eaf5b28a
 
 function OfferSlider() {
   const sliderRef = useRef(null);
 
   const scrollLeft = () => {
-    sliderRef.current.scrollBy({ left: -300, behavior: "smooth" });
+    sliderRef.current?.scrollBy({
+      left: -300,
+      behavior: "smooth",
+    });
   };
 
   const scrollRight = () => {
-    sliderRef.current.scrollBy({ left: 300, behavior: "smooth" });
+    sliderRef.current?.scrollBy({
+      left: 300,
+      behavior: "smooth",
+    });
   };
+
+  const offers = [logo1, logo2, logo3, logo4, logo5, logo6];
 
   return (
     <div className="offer-wrapper">
-
       {/* Video Banner */}
       <div className="offer-video">
-<<<<<<< HEAD
         <video src={video} autoPlay muted loop />
-=======
-        <video src={Fvid} autoPlay muted loop />
->>>>>>> 5d060767d82e39a89ca2bb7ef288f5a8eaf5b28a
-
-
       </div>
 
       {/* Slider */}
       <div className="slider-container">
-        <button className="arrow-left" onClick={scrollLeft}>‹</button>
+        <button
+          className="arrow-left"
+          onClick={scrollLeft}
+          aria-label="Previous offers"
+        >
+          ‹
+        </button>
 
         <div className="slider" ref={sliderRef}>
-<<<<<<< HEAD
-  {[logo1, logo2, logo3, logo4, logo5, logo6].map((img, i) => (
-    <div className="offer-card" key={i}>
-      <img src={img} alt={`offer-${i + 1}`} />
-    </div>
-  ))}
-</div>
-=======
-          {[logo ,logo ,logo ,logo ,logo ].map((img, i) => (
+          {offers.map((img, i) => (
             <div className="offer-card" key={i}>
-              <img src={logo } alt="offer" />
+              <img src={img} alt={`offer-${i + 1}`} />
             </div>
           ))}
         </div>
->>>>>>> 5d060767d82e39a89ca2bb7ef288f5a8eaf5b28a
 
-        <button className="arrow-right" onClick={scrollRight}>›</button>
+        <button
+          className="arrow-right"
+          onClick={scrollRight}
+          aria-label="Next offers"
+        >
+          ›
+        </button>
       </div>
-
     </div>
   );
 }
